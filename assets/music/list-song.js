228a9bae7ab8ec7fb08c3d56126d65691fa6d2f0
Nguyễn Hoàng Nhân <hoangnhan123vnz@gmail.com>
2022-08-15 17:04:37 +0700

    commit

--- assets/music/list-song.js
+++ assets/music/list-song.js
@@ -2,74 +2,26 @@ const songs = [
     {
         name: 'Ghé Qua',
         singer: 'Dick x PC x Tofu',
-        path: './assets/music/Ghe Qua - Dick x PC x Tofu.mp3',
-        image: './assets/img/GheQua.jpg'
+        path: '@/assets/music/Ghe Qua - Dick x PC x Tofu.mp3',
+        image: 'GheQua.jpg'
     },
     {
         name: 'Nắng',
         singer: 'MANBO_ CHANEE',
-        path: './assets/music/Nang - MANBO_ CHANEE.flac',
-        image: './assets/img/Nang.jpg'
+        path: '@/assets/music/Nang - MANBO_ CHANEE.flac',
+        image: 'Nang.jpg'
     },
     {
         name: 'Sao em lại tắt máy',
         singer: 'Pham Nguyen Ngoc',
-        path: './assets/music/Sao Em Lai Tat May_ - Pham Nguyen Ngoc_.flac',
-        image: './assets/img/SaoEmLaiTatMay.jpg'
+        path: '@/assets/music/Sao Em Lai Tat May_ - Pham Nguyen Ngoc_.flac',
+        image: 'SaoEmLaiTatMay.jpg'
     },
     {
         name: 'Thu Cuối',
         singer: 'Yanbi_ Mr_T_ Hang BingBoong',
-        path: './assets/music/Thu Cuoi - Yanbi_ Mr_T_ Hang BingBoong.flac',
-        image: './assets/img/ThuCuoi.jpg'
-    },
-    {
-        name: 'Ghé Qua',
-        singer: 'Dick x PC x Tofu',
-        path: './assets/music/Ghe Qua - Dick x PC x Tofu.mp3',
-        image: './assets/img/GheQua.jpg'
-    },
-    {
-        name: 'Nắng',
-        singer: 'MANBO_ CHANEE',
-        path: './assets/music/Nang - MANBO_ CHANEE.flac',
-        image: './assets/img/Nang.jpg'
-    },
-    {
-        name: 'Sao em lại tắt máy',
-        singer: 'Pham Nguyen Ngoc',
-        path: './assets/music/Sao Em Lai Tat May_ - Pham Nguyen Ngoc_.flac',
-        image: './assets/img/SaoEmLaiTatMay.jpg'
-    },
-    {
-        name: 'Thu Cuối',
-        singer: 'Yanbi_ Mr_T_ Hang BingBoong',
-        path: './assets/music/Thu Cuoi - Yanbi_ Mr_T_ Hang BingBoong.flac',
-        image: './assets/img/ThuCuoi.jpg'
-    },
-    {
-        name: 'Ghé Qua',
-        singer: 'Dick x PC x Tofu',
-        path: './assets/music/Ghe Qua - Dick x PC x Tofu.mp3',
-        image: './assets/img/GheQua.jpg'
-    },
-    {
-        name: 'Nắng',
-        singer: 'MANBO_ CHANEE',
-        path: './assets/music/Nang - MANBO_ CHANEE.flac',
-        image: './assets/img/Nang.jpg'
-    },
-    {
-        name: 'Sao em lại tắt máy',
-        singer: 'Pham Nguyen Ngoc',
-        path: './assets/music/Sao Em Lai Tat May_ - Pham Nguyen Ngoc_.flac',
-        image: './assets/img/SaoEmLaiTatMay.jpg'
-    },
-    {
-        name: 'Thu Cuối',
-        singer: 'Yanbi_ Mr_T_ Hang BingBoong',
-        path: './assets/music/Thu Cuoi - Yanbi_ Mr_T_ Hang BingBoong.flac',
-        image: './assets/img/ThuCuoi.jpg'
+        path: '@/assets/music/Thu Cuoi - Yanbi_ Mr_T_ Hang BingBoong.flac',
+        image: 'ThuCuoi.jpg'
     }
 ];
 
