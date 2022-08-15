99d542ca6c631544d01aa4d913e211b335ec0670
Nguyễn Hoàng Nhân <hoangnhan123vnz@gmail.com>
2022-08-15 10:20:57 +0700

    Add folder
    Add new assets folder

---
+++ assets/music/list-song.js
@@ -0,0 +1,76 @@
+const songs = [
+    {
+        name: 'Ghé Qua',
+        singer: 'Dick x PC x Tofu',
+        path: './assets/music/Ghe Qua - Dick x PC x Tofu.mp3',
+        image: './assets/img/GheQua.jpg'
+    },
+    {
+        name: 'Nắng',
+        singer: 'MANBO_ CHANEE',
+        path: './assets/music/Nang - MANBO_ CHANEE.flac',
+        image: './assets/img/Nang.jpg'
+    },
+    {
+        name: 'Sao em lại tắt máy',
+        singer: 'Pham Nguyen Ngoc',
+        path: './assets/music/Sao Em Lai Tat May_ - Pham Nguyen Ngoc_.flac',
+        image: './assets/img/SaoEmLaiTatMay.jpg'
+    },
+    {
+        name: 'Thu Cuối',
+        singer: 'Yanbi_ Mr_T_ Hang BingBoong',
+        path: './assets/music/Thu Cuoi - Yanbi_ Mr_T_ Hang BingBoong.flac',
+        image: './assets/img/ThuCuoi.jpg'
+    },
+    {
+        name: 'Ghé Qua',
+        singer: 'Dick x PC x Tofu',
+        path: './assets/music/Ghe Qua - Dick x PC x Tofu.mp3',
+        image: './assets/img/GheQua.jpg'
+    },
+    {
+        name: 'Nắng',
+        singer: 'MANBO_ CHANEE',
+        path: './assets/music/Nang - MANBO_ CHANEE.flac',
+        image: './assets/img/Nang.jpg'
+    },
+    {
+        name: 'Sao em lại tắt máy',
+        singer: 'Pham Nguyen Ngoc',
+        path: './assets/music/Sao Em Lai Tat May_ - Pham Nguyen Ngoc_.flac',
+        image: './assets/img/SaoEmLaiTatMay.jpg'
+    },
+    {
+        name: 'Thu Cuối',
+        singer: 'Yanbi_ Mr_T_ Hang BingBoong',
+        path: './assets/music/Thu Cuoi - Yanbi_ Mr_T_ Hang BingBoong.flac',
+        image: './assets/img/ThuCuoi.jpg'
+    },
+    {
+        name: 'Ghé Qua',
+        singer: 'Dick x PC x Tofu',
+        path: './assets/music/Ghe Qua - Dick x PC x Tofu.mp3',
+        image: './assets/img/GheQua.jpg'
+    },
+    {
+        name: 'Nắng',
+        singer: 'MANBO_ CHANEE',
+        path: './assets/music/Nang - MANBO_ CHANEE.flac',
+        image: './assets/img/Nang.jpg'
+    },
+    {
+        name: 'Sao em lại tắt máy',
+        singer: 'Pham Nguyen Ngoc',
+        path: './assets/music/Sao Em Lai Tat May_ - Pham Nguyen Ngoc_.flac',
+        image: './assets/img/SaoEmLaiTatMay.jpg'
+    },
+    {
+        name: 'Thu Cuối',
+        singer: 'Yanbi_ Mr_T_ Hang BingBoong',
+        path: './assets/music/Thu Cuoi - Yanbi_ Mr_T_ Hang BingBoong.flac',
+        image: './assets/img/ThuCuoi.jpg'
+    }
+];
+
+export default songs;
